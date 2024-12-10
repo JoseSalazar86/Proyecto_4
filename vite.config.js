8be5433d944e56ee2270cd4abd40a77d9cbd4f30
JoseSalazar86 <48056211+JoseSalazar86@users.noreply.github.com>
2024-12-10 10:55:23 +0100

    v6

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'es',
+        assetFileNames: `[name].[ext]`
+      }
+    },
+    // Configurar MIME manualmente para evitar problemas en GitHub Pages
+    server: {
+      mimeTypes: {
+        'application/javascript': ['js', 'jsx']
+      }
+    }
+  }
+});
